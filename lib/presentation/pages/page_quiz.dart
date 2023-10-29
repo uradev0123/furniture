96da3249cc64dd022f23f89a5b6dd95b5127d800
tarobee <sumoka0325@ezweb.ne.jp>
2023-10-29 13:49:54 +0900

    create quiz page looks

--- lib/presentation/pages/page_quiz.dart
+++ lib/presentation/pages/page_quiz.dart
@@ -2,6 +2,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:furniture/application/state/quiz/index/index.dart';
+import 'package:furniture/presentation/widgets/my_widgets.dart';
 
 
 @RoutePage()
@@ -13,36 +14,48 @@ class PageQuiz extends ConsumerWidget {
     // watch, listenはbuild(ref)直下で使う
     final index = ref.watch(indexNotifierProvider);
 
-    // S1 listen
-    ref.listen(
-      indexNotifierProvider,
-          (oldState, newState) {
-        // スナックバーを表示
-        ScaffoldMessenger.of(context).showSnackBar(
-          SnackBar(
-            content: Text('$oldStateから$newStateへ変更されました'),
-          ),
-        );
-      },
+    // // S1 listen
+    // ref.listen(
+    //   indexNotifierProvider,
+    //       (oldState, newState) {
+    //     // スナックバーを表示
+    //     ScaffoldMessenger.of(context).showSnackBar(
+    //       SnackBar(
+    //         content: Text('$oldStateから$newStateへ変更されました'),
+    //       ),
+    //     );
+    //   },
+    // );
+
+    const details = 'Yチェア\nハンス・J・ウェグナー\nカールハンセン&サン\n1949年設立';
+    final indexText = TestText(details);
+    final image = ImageL(Image.network('https://user0514.cdnw.net/shared/img/thumb/shikunDJI_0096-NR233_TP_V.jpg'));
+    final answerButton = ButtonL(
+      text: '答え',
+      onPressed: () {},
+    );
+    final nextButton = ButtonL(
+      text: '次へ',
+      onPressed: () {},
     );
 
-    final indexText = Text('$index');
-    final indexButton = ElevatedButton(
-      onPressed: () {
-        // indexノティファイアを呼ぶ（build(ref)から更にネストしたスコープの場合はwatch, listenではなくreadを使う）
-        final notifier = ref.read(indexNotifierProvider.notifier);
-        // indexデータを変更
-        notifier.updateState();
-      },
-      child: const Text('+1'),
-    );
+    // final indexButton = ElevatedButton(
+    //   onPressed: () {
+    //     // indexノティファイアを呼ぶ（build(ref)から更にネストしたスコープの場合はwatch, listenではなくreadを使う）
+    //     final notifier = ref.read(indexNotifierProvider.notifier);
+    //     // indexデータを変更
+    //     notifier.updateState();
+    //   },
+    //   child: const Text('+1'),
+    // );
 
     // 縦に並べる
     return Column(
       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
       children: [
+        image,
         indexText,
-        indexButton,
+        answerButton,
       ],
     );
   }
---
+++ lib/presentation/widgets/my_widgets.dart
@@ -0,0 +1,3 @@
+export 'package:furniture/presentation/widgets/image.dart';
+export 'package:furniture/presentation/widgets/text.dart';
+export 'package:furniture/presentation/widgets/button.dart';
