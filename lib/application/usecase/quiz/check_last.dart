7008d6ce5eddfb57416aa590bebb48be0823709e
tarobee <sumoka0325@ezweb.ne.jp>
2023-10-30 23:31:50 +0900

    adjust usecase, but I don't have succeeds especially

--- lib/application/usecase/quiz/check_last.dart
+++ lib/application/usecase/quiz/check_last.dart
@@ -2,12 +2,8 @@ import 'package:furniture/domain/types/types.dart';
 import 'package:flutter/material.dart';
 
 class CheckLastUsecase {
-  CheckLastUsecase({required this.index, required this.list});
 
-  int index;
-  List<Furniture> list;
-
-  bool checkLast() {
+  bool checkLast(int index, List<Furniture> list) {
     final lastIndex = list.length - 1;
     if(index < lastIndex) { return false; }
     else if(index == lastIndex) { return true; }
--- lib/application/usecase/quiz/next_question.dart
+++ lib/application/usecase/quiz/next_question.dart
@@ -1,45 +1,30 @@
-// import 'package:furniture/application/state/quiz/state.dart';
-// import 'package:furniture/application/usecase/quiz/quiz_usecase.dart';
-// import 'package:furniture/domain/types/types.dart';
-// import 'package:flutter/material.dart';
-// import 'package:furniture/presentation/dialogs/my_dialogs.dart';
-//
-// class NextQuestionUsecase {
-//   NextQuestionUsecase({
-//     required this.index,
-//     required this.list,
-//     required this.indexNotifier
-//
-//   });
-//
-//   int index;
-//   List<Furniture> list;
-//   IndexNotifier indexNotifier;
-//
-//
-//
-//
-//
-//   void nextQuestion() {
-//     // インデックスを1増加させる
-//     final indexNotifier = ref.read(indexNotifierProvider.notifier);
-//     indexNotifier.nextIndex();
-//     // 問題を更新
-//     setCurrentQuestion();
-//   }
-//
-//   bool checkLast(int index, List<Furniture> list) {
-//     final lastIndex = list.length - 1;
-//     if (index < lastIndex) {
-//       return false;
-//     }
-//     else if (index == lastIndex) {
-//       return true;
-//     }
-//     else {
-//       debugPrint('page_quiz.dart CheckLast error インデックスがおかしい');
-//       return true;
-//     }
-//   }
-//
-// }
+import 'package:furniture/application/state/quiz/state.dart';
+import 'package:furniture/application/usecase/quiz/quiz_usecase.dart';
+import 'package:furniture/domain/types/types.dart';
+import 'package:flutter/material.dart';
+import 'package:furniture/presentation/dialogs/my_dialogs.dart';
+
+class NextQuestionUsecase {
+  NextQuestionUsecase({
+    required this.indexNotifier,
+    required this.detailsNotifier,
+    required this.imageNotifier,
+    required this.isQuestionNotifier,
+  });
+  
+  IndexNotifier indexNotifier;
+  DetailsNotifier detailsNotifier;
+  ImageNotifier imageNotifier;
+  IsQuestionNotifier isQuestionNotifier;
+  
+  
+  void nextQuestion() {
+    indexNotifier.nextIndex();
+    final usecase = UpdateQuestionUsecase(
+        detailsNotifier: detailsNotifier, 
+        imageNotifier: imageNotifier, 
+        isQuestionNotifier: isQuestionNotifier
+    );
+
+  }
+}
