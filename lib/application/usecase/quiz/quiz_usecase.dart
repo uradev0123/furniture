ce71eed6c08caf22ea4ac809bdbbd44bad78b2a4
tarobee <sumoka0325@ezweb.ne.jp>
2023-10-31 11:18:05 +0900

    use methods in usecase

--- lib/application/usecase/quiz/quiz_usecase.dart
+++ lib/application/usecase/quiz/quiz_usecase.dart
@@ -1,3 +1,4 @@
 export 'package:furniture/application/usecase/quiz/next_question.dart';
 export 'package:furniture/application/usecase/quiz/show_answer.dart';
 export 'package:furniture/application/usecase/quiz/update_question.dart';
+export 'package:furniture/application/usecase/quiz/restart.dart';
