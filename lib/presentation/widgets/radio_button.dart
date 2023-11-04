d56ec8636d145b15327123b48ad1e04dcdc0c98e
uradev0123 <uradev0123@gmail.com>
2023-11-05 07:52:43 +0900

    create quiz select menu format

--- lib/presentation/widgets/radio_button.dart
+++ lib/presentation/widgets/radio_button.dart
@@ -30,3 +30,33 @@ class RadioButtonRow extends StatelessWidget {
     );
   }
 }
+
+class RadioButtonColumn extends StatelessWidget {
+  const RadioButtonColumn({
+    required this.id,
+    required this.onChanged,
+    required this.values,
+    super.key
+  });
+
+  final dynamic id;
+  final List<dynamic> values;
+  final void Function(dynamic value) onChanged;
+
+  @override
+  Widget build(BuildContext context) {
+    final radioTiles = values.map((e) {
+      return RadioListTile(
+        groupValue: id,
+        title: Text('$e'),
+        value: e,
+        onChanged: onChanged,
+      );
+    }).toList();
+
+    return Column(
+      children: radioTiles,
+    );
+  }
+}
+
